45e19fb82b19ab0232675a54c938d0c56eb004fc
Monkey <mr.monkey.job@gmail.com>
2023-06-15 18:31:00 +0800

    update Contact send

--- media.go
+++ media.go
@@ -146,7 +146,7 @@ type Document struct {
 	// (Optional)
 	Thumbnail            *Photo `json:"thumbnail,omitempty"`
 	Caption              string `json:"caption,omitempty"`
-	MIME                 string `json:"mime_type"`
+	MIME                 string `json:"mime_type,omitempty"`
 	FileName             string `json:"file_name,omitempty"`
 	DisableTypeDetection bool   `json:"disable_content_type_detection,omitempty"`
 }
@@ -300,12 +300,16 @@ func (s *Sticker) MediaFile() *File {
 
 // Contact object represents a contact to Telegram user.
 type Contact struct {
+	// Contact's phone number
 	PhoneNumber string `json:"phone_number"`
-	FirstName   string `json:"first_name"`
-
-	// (Optional)
-	LastName string `json:"last_name"`
-	UserID   int64  `json:"user_id,omitempty"`
+	// Contact's first name
+	FirstName string `json:"first_name"`
+	// Optional. Contact's last name
+	LastName string `json:"last_name,omitempty"`
+	// Optional. Contact's user identifier in Telegram. This number may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a 64-bit integer or double-precision float type are safe for storing this identifier.
+	UserId int64 `json:"user_id,omitempty"`
+	// Optional. Additional data about the contact in the form of a vCard
+	Vcard string `json:"vcard,omitempty"`
 }
 
 // Location object represents geographic position.
